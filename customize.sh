a23c0263614682ab3950a88090b676ee89eaebfc
Marius Kopp <mariuskopp517@gmail.com>
2020-01-22 16:20:12 +0100

    V.SIX TEST
    Switched to MMT-Extended Template. Test!

--- customize.sh
+++ customize.sh
@@ -1,47 +1,17 @@
-##########################################################################################
 #
-# MMT Extended Config Script
+# MMT EXTENDED XPIXELERIA CONFIG SCRIPT
 #
-##########################################################################################
 
-##########################################################################################
-# Config Flags
-##########################################################################################
-
-# Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
-# Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
-# Uncomment DEBUG if you want full debug logs (saved to /sdcard)
-#MINAPI=21
+MINAPI=29
 #MAXAPI=25
 #DYNLIB=true
 #DEBUG=true
 
-##########################################################################################
-# Replace list
-##########################################################################################
-
-# List all directories you want to directly replace in the system
-# Check the documentations for more info why you would need this
-
-# Construct your list in the following format
-# This is an example
-REPLACE_EXAMPLE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
-# Construct your own list here
 REPLACE="
 "
 
-##########################################################################################
-# Permissions
-##########################################################################################
-
 set_permissions() {
-  : # Remove this if adding to this function
+  : # REMOVE THIS IF ADDING TO THIS FUNCTION
 
   # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
   # Some examples:
@@ -59,9 +29,9 @@ set_permissions() {
   # set_perm /data/local/tmp/file.txt 0 0 644
 }
 
-##########################################################################################
-# MMT Extended Logic - Don't modify anything after this
-##########################################################################################
+#
+# MMT EXTENDED LOGIC - DON'T MODIFY ANYTHING AFTER THIS
+#
 
 SKIPUNZIP=1
 unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
